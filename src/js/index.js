5756f871e76c7999b504a7de43394280b56232a1
caamomilaa <c.velazquezdamico@gmail.com>
2024-06-26 00:31:32 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const passwordElement = document.getElementById("password");
+const passwordGeneratorElement = document.getElementById("input");
+const passwordLengthElement = document.getElementById("label");
+const buttonElement = document.getElementById("button");
+
+const passwordCharacters = ["a", "b", "c", "1", "2", "3"];
+
+const lengthValue = (event) => {
+  passwordLengthElement.textContent = `Length: ${passwordGeneratorElement.value}`;
+};
+passwordGeneratorElement.addEventListener("input", lengthValue);
+
+//- Conseguir que se genere un string de esa longitud al pulsar el botón y se escriba en el primer input.
+
+//length [input.length] - escucho el botón - const: textcontent= palabras random en input inicial.
+
+const randomWord = () => {};
+
+const buttonGenerator = (event) => {
+  //random con length.value
+};
+
+buttonElement.addEventListener("click", buttonGenerator);
