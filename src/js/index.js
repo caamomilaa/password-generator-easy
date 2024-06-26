28fe3b69f2f1dd64cfe39824896d64666f0cf0a6
Ucmessina <163582100+caamomilaa@users.noreply.github.com>
2024-06-26 18:48:25 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,32 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import "../scss/styles.scss";
+import '../scss/styles.scss';
 
-const passwordElement = document.getElementById("password");
-const passwordGeneratorElement = document.getElementById("input");
-const passwordLengthElement = document.getElementById("label");
-const buttonElement = document.getElementById("button");
+const passwordElement = document.getElementById('password');
+const passwordGeneratorElement = document.getElementById('input');
+const passwordLengthElement = document.getElementById('length-text');
+const buttonElement = document.getElementById('button');
 
-const passwordCharacters = ["a", "b", "c", "1", "2", "3"];
+const passwordCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
 
-const lengthValue = (event) => {
-  passwordLengthElement.textContent = `Length: ${passwordGeneratorElement.value}`;
+const lengthValue = event => {
+  passwordLengthElement.textContent = passwordGeneratorElement.value;
 };
-passwordGeneratorElement.addEventListener("input", lengthValue);
+passwordGeneratorElement.addEventListener('input', lengthValue);
 
 //- Conseguir que se genere un string de esa longitud al pulsar el botón y se escriba en el primer input.
 
 //length [input.length] - escucho el botón - const: textcontent= palabras random en input inicial.
 
-const randomWord = () => {};
-
-const buttonGenerator = (event) => {
-  //random con length.value
+const randomWord = () => {
+  let finalPassword = '';
+  for (let i = 0; i < passwordGeneratorElement.value; i++) {
+    finalPassword += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
+  }
+  return finalPassword;
 };
 
-buttonElement.addEventListener("click", buttonGenerator);
+const buttonGenerator = () => {
+  passwordElement.value = randomWord();
+};
+
+buttonElement.addEventListener('click', buttonGenerator);
